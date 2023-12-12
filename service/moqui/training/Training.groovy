1a98cacf39f0fc2c9d237fe90d21f3a17c40dfe1
Nutan Shinde <shindenutan015@gmail.com>
2023-12-12 20:30:45 +0530

    groovy

--- service/moqui/training/Training.groovy
+++ service/moqui/training/Training.groovy
@@ -1,4 +1,4 @@
 def training = ec.entity.makeValue("moqui.training.MoquiTraining")
 training.setFields(context, true, null, null)
-//training.setSequencedIdPrimary()
+training.setSequencedIdPrimary()
 training.create()
